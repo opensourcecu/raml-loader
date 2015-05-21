1a79e733c8454a883e71365cd808102ff27969db
Matt Dean <matt@trabian.com>
2015-05-21 15:25:05 -0400

    Mark dependencies
    Invalidate cache and recompile in watch mode

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var raml = require('raml-parser');
 module.exports = function(content) {
 
   var cb = this.async();
+  var self = this;
 
   this.cacheable();
+
+  // Patch into getPendingFile to add dependencies when visited by the parser.
+  var getPendingFile = raml.RamlParser.prototype.getPendingFile;
+
+  raml.RamlParser.prototype.getPendingFile = function(loader, fileInfo) {
+    self.addDependency(fileInfo.targetFileUri);
+    return getPendingFile.call(this, loader, fileInfo);
+  };
 
   raml.load(content, this.resourcePath).then(function(data) {
     cb(null, "module.exports = " + JSON.stringify(data));
